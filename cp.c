4d66458da7eb40e9cfa7b8ba301bb9f093c71978
MRwaffle-14th <eitanerez4321@gmail.com>
2026-04-13 20:59:16 +0300

    cp is better now. cleaner.

---
+++ cp.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s <src> <dest>\n", argv[0]);
+        return 1;
+    }
+
+    int src = open(argv[1], O_RDONLY);
+
+    if (src < 0)
+    {
+        perror("open src");
+        return 1;
+    }
+
+    int dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (dst < 0)
+    {
+        perror("open dest");
+        close(src);
+        return 1;
+    }
+
+    char buf[8192];
+    ssize_t n;
+
+    while ((n = read(src, buf, sizeof(buf))) > 0)
+    {
+        ssize_t written = write(dst, buf, n);
+        if (written != n)
+        {
+            perror("write");
+            close(src);
+            close(dst);
+            return 1;
+        }
+    }
+
+    if (n < 0)
+    {
+        perror("read");
+    }
+
+    close(src);
+    close(dst);
+
+    return 0;
+}
